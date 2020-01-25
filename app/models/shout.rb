aa9515d167300166265bb48ec87d2bb9e66a90d9
Anthony Diké <anthonydike@nyu.edu>
2020-01-25 18:02:29 -0500

    Delegated username data to shout model to highlight relationship between a user and a shout

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -5,4 +5,8 @@ class Shout < ApplicationRecord
   validates :user, presence: true 
 
   default_scope { order(created_at: :desc)}
+
+  # highlights that username is a relationship between the user and a shout
+  # provides additional context
+  delegate :username, to: :user
 end
